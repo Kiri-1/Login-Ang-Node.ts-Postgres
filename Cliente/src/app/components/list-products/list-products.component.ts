9044586c27dca4314c765e10f09b500abbec2dd5
Kiri-1 <belenjesuslp@gmail.com>
2024-10-13 18:09:05 -0300

    navbar y lista

--- Cliente/src/app/components/list-products/list-products.component.ts
+++ Cliente/src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-list-products',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent {
+  listProducts: Product[] = [
+    { name: 'Pesi', description: 'rica', price: 400, stock: 500 },
+    { name: 'Corona', description: 'sobrevalorada', price: 900, stock: 50 },
+    { name: 'Bramma', description: 'sabroza', price: 300, stock: 700 }
+
+  ];
 
 }
