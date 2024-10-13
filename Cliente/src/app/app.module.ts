655d8998b45c6e3b47239217211f1fa2012422a5
Kiri-1 <belenjesuslp@gmail.com>
2024-10-13 11:50:19 -0300

    creando CARPETAS Y ARCHIVOS

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductsComponent,
+    AddEditProductComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
